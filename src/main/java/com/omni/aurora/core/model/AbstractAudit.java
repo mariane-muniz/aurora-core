7d15dee14b4b34bec2b2d99b2a2e8f23d27740f9
Mariane Muniz <mariane.muniz.ribeiro@hotmail.com>
2020-03-19 20:59:41 -0300

    update

---
+++ src/main/java/com/omni/aurora/core/model/AbstractAudit.java
@@ -0,0 +1,32 @@
+package com.omni.aurora.core.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(
+        value = {"createdAt", "updatedAt"},
+        allowGetters = true
+)
+public abstract class AbstractAudit implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private Date updatedAt;
+}
